a0f4387af10bff6717b9470e11d4479b939c2b41
hwiju yeom <yeom456@gmail.com>
2022-01-27 15:45:06 +0900

    Update App.js

--- ReactJS로 영화 웹 서비스 만들기/5.Effect/effects/src/App.js
+++ ReactJS로 영화 웹 서비스 만들기/5.Effect/effects/src/App.js
@@ -1,21 +1,40 @@
-import {useState,useEffect} from 'react';
+import { useEffect, useState } from "react";
 
-function Hello(){
-  useEffect(()=>{
-    console.log("bye :<");
-    return () => console.log("create :>");
-  },[]);
-  return<h1>Hello</h1>;
-}
-
-//js 쓸 때 중괄호 !! {} !!! 
 function App() {
-  const [showing,setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
