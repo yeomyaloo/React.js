6ea768bf8097cf984524f811441bc4ad615c4657
hwiju yeom <yeom456@gmail.com>
2022-01-31 14:54:06 +0900

    1/31
    라우터기능에 대해서 알아봄
    다음은 동적 라우터에 대해서 알아볼 예정

--- ReactJS로 영화 웹 서비스 만들기/5.Effect/effects/src/App.js
+++ ReactJS로 영화 웹 서비스 만들기/5.Effect/effects/src/App.js
@@ -1,41 +1,20 @@
-import { useEffect, useState } from "react";
+import React from "react";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+import {
+  Browser as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
   return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map((g) => (
-                  <li key={g}>{g}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+  <Router> 
+    <Routes>
+      <Route path="movie" element={<Detail />}/>
+      <Route path="/" element ={<Home/>}/>
+    </Routes>
+  </Router>
   );
 }
 
