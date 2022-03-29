91d2576e4e9484bbb8c18bb646639f0890e24d2d
hwiju yeom <yeom456@gmail.com>
2022-03-29 21:06:59 +0900

    3/29

--- do-it-example/src/06/ButtonWithNewConsumer.jsx
+++ do-it-example/src/06/ButtonWithNewConsumer.jsx
@@ -3,21 +3,32 @@ import PropTypes from 'prop-types';
 import Button from '../04/Button';
 import { Consumer } from './LoadingProviderWithNewContext';
 
-function ButtonWithNewConsumer({ children }) {
+function ButtonWithNewConsumer({ label }) {
   return (
     <React.Fragment>
-      <Consumer
-        children={(value) => (
-          <Button onPress={(() => value.setLoading('loading'), !value.loading)}>
-            {value.loading ? '로딩 중' : children}
+      <Consumer>
+        {(value) => (
+          <Button onPress={() => value.setLoading('loading', !value.loading)}>
+            {value.loading ? '로딩중' : label}
           </Button>
         )}
-      />
-      <Consumer
-        children={({ loading = false, loading2 = false }) => (
-          <Button>{loading && loading2 ? '로딩 중' : children}</Button>
+      </Consumer>
+      <Consumer>
+        {({ loading2, setLoading }) => (
+          <Button onPress={() => setLoading('loading2', !loading2)}>
+            {loading2 ? '로딩중' : label}
+          </Button>
         )}
-      />
+      </Consumer>
+      <Consumer>
+        {({ loading, loading2 }) => <Button>{loading && loading2 ? '로딩중' : label}</Button>}
+      </Consumer>
     </React.Fragment>
   );
 }
+
+ButtonWithNewConsumer.propTypes = {
+  label: PropTypes.string,
+};
+
+export default ButtonWithNewConsumer;
