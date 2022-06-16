3ef227da77086730297f66a7c481def1f0afd7bf
hwiju yeom <yeom456@gmail.com>
2022-06-16 22:49:28 +0900

    6/16
    front
    1. 리스트 작성기본 툴
    2. 리스트 생성, 삭제, 수정

--- TodoList/react-workspace/todo-react-app/src/AddTodo.js
+++ TodoList/react-workspace/todo-react-app/src/AddTodo.js
@@ -5,17 +5,46 @@ class AddTodo extends React.Component {
   constructor(props){
     super(props);
     this.state = {item: {title: ""}}; /* 사용자 입력을 저장할 오브젝트 */
+    this.add = props.add; // props의 함수를 this.add에 연결
+  }
+
+  onInputChange = (e) => { //함수 작성
+    const thisItem = this.state.item;
+    thisItem.title = e.target.value;
+    this.setState({item:thisItem});
+    console.log(thisItem);
+  }
+
+  onButtonClick= (e) => {
+    this.add(this.state.item);//add함수 사용
+    this.setState({item:{title: ""}});
+  }
+  enterKeyEventHandler = (e) => {
+    if(e.key === 'Enter'){
+      this.onButtonClick();
+    }
   }
 
   render() {
+    //함수 연결
     return (
       <Paper style={{margin:16, padding:16}}>
         <Grid container>
           <Grid xs={11} md={11} item style={{paddingRight:16}}>
-            <TextField placeholder='Add Todo here' fullWidth />
+            <TextField 
+              placeholder='Add Todo here' 
+              fullWidth
+              onChange={this.onInputChange}
+              value ={this.state.item.title}
+              onKeyPress={this.enterKeyEventHandler}
+            />
           </Grid>
           <Grid xs={1} md={1} item>
-            <Button fullWidth color='secondary' variant='outline'>
+            <Button 
+              fullWidth 
+              color='secondary' 
+              variant='outline'
+              onClick={this.onButtonClick}>
               +
             </Button>
           </Grid>
--- TodoList/react-workspace/todo-react-app/src/App.js
+++ TodoList/react-workspace/todo-react-app/src/App.js
@@ -14,20 +14,42 @@ class App extends React.Component {
       ],
     };
   }
+
+  // add() 함수 추가
+  add = (item) => {
+    const thisItems = this.state.items;
+    item.id = "ID" + thisItems.length; // key를 위한 id 추가
+    item.done = false; // done 초기화
+    thisItems.push(item); // 리스트에 아이템 추가
+    this.setState({items:thisItems}); //업데이트의 경우엔 반드시 this.setState로 진행
+    console.log("items: ", this.state.items); 
+  } 
+
+  delete = (item) => {
+    const thisItems = this.state.items;
+    console.log("Before Update Items: ", this.state.items)
+    const newItems = thisItems.filter(e => e.id !== item.id);
+    this.setState({items: newItems}, ()=>{
+      //디버깅 콜백
+      console.log("Update Items: ", this.state.items)
+    })
+  }
   render() {
     var todoItems = this.state.items.length > 0 && (
       <Paper style={{margin:16}}>
         <List>
           {this.state.items.map((item,idx)=>(
-            <Todo item ={item} key ={item.id}/>
+            <Todo item ={item} key ={item.id} delete = {this.delete} />
           ))}
         </List>
       </Paper>
     );
+
+    //함수 연결
     return (
         <div className='App'>
           <Container maxWidth="md">
-            <AddTodo/>
+            <AddTodo add={this.add} />
             <div className='TodoList'>{todoItems}</div>
           </Container>
         </div>
